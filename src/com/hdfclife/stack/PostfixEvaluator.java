1ec40330f5f094ab17961f6a69a5137a879b8d24
YOGRAJ G S <yograjgs4@gmail.com>
2026-09-05 12:52:30 +0530

    Update PostfixEvaluator.java

--- src/com/hdfclife/stack/PostfixEvaluator.java
+++ src/com/hdfclife/stack/PostfixEvaluator.java
@@ -5,18 +5,13 @@ public class PostfixEvaluator {
     public static int evaluate(
             String expression) {
 
-        LinkedClaimStack stack =
-                new LinkedClaimStack();
+        LinkedClaimStack stack =new LinkedClaimStack();
 
-        String[] values =
-                expression.split(" ");
+        String[] values =expression.split(" ");
 
         for (String value : values) {
 
-            if (value.equals("+") ||
-                    value.equals("-") ||
-                    value.equals("*") ||
-                    value.equals("/")) {
+            if (value.equals("+") ||value.equals("-") ||value.equals("*") ||value.equals("/")) {
 
                 int second = stack.pop();
 
@@ -24,37 +19,28 @@ public class PostfixEvaluator {
 
                 if (value.equals("+")) {
 
-                    stack.push(
-                            first + second
-                    );
+                    stack.push(first + second);
                 }
 
                 if (value.equals("-")) {
 
-                    stack.push(
-                            first - second
-                    );
+                    stack.push(first - second);
                 }
 
                 if (value.equals("*")) {
 
-                    stack.push(
-                            first * second
-                    );
+                    stack.push(first * second);
                 }
 
                 if (value.equals("/")) {
 
-                    stack.push(
-                            first / second
-                    );
+                    stack.push(first / second);
                 }
 
-            } else {
+            } 
+            else {
 
-                stack.push(
-                        Integer.parseInt(value)
-                );
+                stack.push(Integer.parseInt(value));
             }
         }
 
